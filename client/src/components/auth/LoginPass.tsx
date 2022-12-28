8335df69769822379a1e1a4b446a35260219699d
tuanminh2 <aroi671620@gmail.com>
2022-12-28 15:54:09 +0700

    complete form login v8

---
+++ client/src/components/auth/LoginPass.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { InputChange } from "../../TypeScript";
+const LoginPass = () => {
+  const initState = { account: "", password: "" };
+
+  const [userLogin, setUserLogin] = useState(initState);
+  const { account, password } = userLogin;
+  const [typePass, setTypePass] = useState(false);
+  const hdlChangInput = (e: InputChange) => {
+    const { value, name } = e.target;
+    setUserLogin({ ...userLogin, [name]: value });
+  };
+
+  return (
+    <form method="post">
+      <div className="form-group">
+        <label htmlFor="account">Email/ Phone/ </label>
+        <input
+          type="text"
+          className="form-control"
+          id="account"
+          name="account"
+          value={account}
+          onChange={hdlChangInput}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">password </label>
+        <div className="pass">
+          <input
+            type={typePass ? "text" : "password"}
+            className="form-control"
+            id="password"
+            name="password"
+            value={password}
+            onChange={hdlChangInput}
+          />
+          <small onClick={() => setTypePass(!typePass)}>
+            {typePass ? "Hide" : "Show"}
+          </small>
+        </div>
+      </div>
+
+      <button
+        type="submit"
+        className="btn btn-dark w-100"
+        disabled={account && password ? false : true}
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginPass;
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import LoginPass from "../components/auth/LoginPass";
 
-const login = () => {
+const Login = () => {
+  const [sms, setSms] = useState(false);
   return (
-    <div>login</div>
-  )
-}
+    <div className="auth_page">
+      <div className="auth_box">
+        <h3 className="text-uppecase text-center mb-4"></h3>
+        <LoginPass />
 
-export default login
+        <small className="row my-2 text-primary" style={{ cursor: "pointer" }}>
+          <Link to="/forot_password" className="col-6">
+            Forgot password?
+          </Link>
+
+          <span className="col-6" onClick={() => setSms(!sms)}>
+            {sms ? "sign in with sms" : "sign in with password"}
+          </span>
+        </small>
+
+        <p>
+          You don't have an account?
+          <Link to="/register"> Register now</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
