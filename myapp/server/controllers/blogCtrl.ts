7ee7ea3175fd48d07d5e61b63b8e0fc18cd96886
tuanminh2 <aroi671620@gmail.com>
2023-01-17 16:18:25 +0700

    populate return object, lk return [], ref can be table name or export name in model

--- myapp/server/controllers/blogCtrl.ts
+++ myapp/server/controllers/blogCtrl.ts
@@ -1,32 +1,78 @@
-import { Request, Response } from 'express'
-import Blogs from '../models/blogModel'
-import { IReqAuth } from '../config/interface'
-
+import { Request, Response } from "express";
+import Blogs from "../models/blogModel";
+import { IReqAuth } from "../config/interface";
 
 const blogCtrl = {
   createBlog: async (req: IReqAuth, res: Response) => {
-    if(!req.user) return res.status(400).json({msg: "Invalid Authentication."})
+    if (!req.user)
+      return res.status(400).json({ msg: "Invalid Authentication." });
 
     try {
-      const { title, content, description, thumbnail, category } = req.body
+      const { title, content, description, thumbnail, category } = req.body;
 
       const newBlog = new Blogs({
         user: req.user._id,
-        title, 
+        title,
         content,
-        description, 
-        thumbnail, 
-        category
-      })
+        description,
+        thumbnail,
+        category,
+      });
 
-      await newBlog.save()
-      res.json({newBlog})
-
+      await newBlog.save();
+      res.json({ newBlog });
     } catch (err: any) {
-      return res.status(500).json({msg: err.message})
+      return res.status(500).json({ msg: err.message });
     }
-  }
-}
+  },
+  getHomeBlogs: async (req: Request, res: Response) => {
+    try {
+      const blogs = await Blogs.aggregate([
+        {
+          $lookup: {
+            from: "users",
+            let: { user_id: "$user" },
+            pipeline: [
+              { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
+              { $project: { password: 0 } },
+            ],
+            as: "user",
+          },
+        },
+      ]);
+      res.json(blogs);
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  getHomeBlogsMy: async (req: Request, res: Response) => {
+    try {
+      //   const blogs = await Blogs.aggregate([
+      //     {
+      //       $lookup: {
+      //         from: "users",
+      //         localField: "user",
+      //         foreignField: "_id",
+      //         as: "user",
+      //       },
+      //     },
+      //   ]);
 
+      const blogs = await Blogs.find().populate("user");
+      res.json(blogs);
+    } catch (err: any) {
+      console.log(err);
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  getHomeBlogs0: async (req: Request, res: Response) => {
+    try {
+      const blogs = await Blogs.find();
+      res.json(blogs);
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
 
 export default blogCtrl;
--- myapp/server/models/userModel.ts
+++ myapp/server/models/userModel.ts
@@ -37,5 +37,4 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
-export default mongoose.model<IUser>("User", userSchema);
+export default mongoose.model("user", userSchema);
