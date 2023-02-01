95a66575e45df9fd49d467035d64b6b92fe678be
tuanminh2 <aroi671620@gmail.com>
2023-02-01 16:09:31 +0700

    chi rerender khi state value khac voi state old value

--- client/src/pages/blogs/[slug].tsx
+++ client/src/pages/blogs/[slug].tsx
@@ -6,12 +6,18 @@ import { IParams, RootStore } from "../../TypeScript";
 import { useNavigate } from "react-router-dom";
 
 //MAGIC FUNDAMENTAL: everything chi dc hay nap lai class khi 1: reload
-//ke ca url thay doi(ma k reload) thi cung khong nap lai class 
+//ke ca url thay doi(ma k reload) thi cung khong nap lai class
 //array blogsCategory se duy tri trong RAM cho den khi reload
 //change state(hay render != voi reload) moi thu se dc giu trong ram
 //state là 1 phần bộ nhớ của app và luôn hiện hữu khi app mở, ARRAY IN RAM
 //reload nói chung là tắt rồi bật lại app nên mất hết và phải nạp lại class
+
+//change State thì chạy lại class(new instace) nhưng state sẽ đc giữ lại
+//chi RE render khi setState("khac voi gia tri state truoc do")
 const BlogByCategory = () => {
+  console.log(
+    "______________________________________________________________________________START"
+  );
   const navigate = useNavigate();
 
   const { categories, blogsCategory } = useSelector(
@@ -25,10 +31,12 @@ const BlogByCategory = () => {
 
   useEffect(() => {
     const category = categories.find((item) => item._id === slug);
+    console.log("SetCategory Id=> Rerender ");
     if (category) setCategoryId(category._id);
   }, [slug, categories]);
 
   useEffect(() => {
+    console.log("CategoryId change=> dispatch new ");
     if (!categoryId) return;
     console.log("AFTER rerender vào đây...");
     if (blogsCategory.every((item) => item.id !== categoryId)) {
@@ -38,6 +46,23 @@ const BlogByCategory = () => {
   }, [categoryId]);
   console.log("instance class function");
   console.log("-----", blogsCategory);
+  console.log(
+    "________________________________________________________________________________END"
+  );
+
+  const change = () => {
+    const months = [
+      "63bd024f138e97372b8600e9",
+      "63bd1033138e97372b860151",
+      "63bf68ea4ae5748fc1327058",
+    ];
+
+    const random = Math.floor(Math.random() * months.length);
+    console.log(random, months[random]);
+    console.log("Click change State dan den rerender");
+    setCategoryId(months[random]);
+  };
+
   return (
     <div>
       current :{slug}
@@ -60,12 +85,7 @@ const BlogByCategory = () => {
       </ul>
       <br />
       <div>{blogsCategory.toString()}</div>
-      <button
-        onClick={() => {
-          console.log("Click change State dan den rerender");
-          setCategoryId("63bd1033138e97372b860151");
-        }}
-      >
+      <button onClick={() => change()}>
         Change state and look xem co nap lai class hay khong
       </button>
     </div>
