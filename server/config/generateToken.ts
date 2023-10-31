b452b49e5583821363ae363336975284fbd22b64
TuanMinh7979 <minhtuanto179@gmail.com>
2023-10-31 20:36:35 +0700

    change

--- server/config/generateToken.ts
+++ server/config/generateToken.ts
@@ -18,10 +18,10 @@ export const generateRefreshToken = (payload: object, res: Response) => {
   res.cookie("refreshtoken", refresh_token, {
 
     sameSite: "none",
-    // secure: true,
+    secure: true,
     // httpOnly: true,
-    // path: `/api/refresh_token`,
-    // maxAge: 30*24*60*60*1000 // 30days
+    path: `/api/refresh_token`,
+    maxAge: 30*24*60*60*1000 // 30days
   });
   return refresh_token;
 };
